186fb029bf967e3a2c33f1c910b6a16e55512956
bls337 <bls337@yahoo.com>
2021-11-02 15:48:27 -0700

    Add access to VolumeSettingsBuilder

--- src/main/java/org/micromanager/lightsheetmanager/internal/DefaultLightSheetManager.java
+++ src/main/java/org/micromanager/lightsheetmanager/internal/DefaultLightSheetManager.java
@@ -4,8 +4,9 @@ import org.micromanager.lightsheetmanager.AutofocusSettings;
 import org.micromanager.lightsheetmanager.LightSheetManager;
 
 public class DefaultLightSheetManager implements LightSheetManager {
+   
    /**
-    * Access to an AutofocusSettingsBuilder.
+    * Access to a AutofocusSettingsBuilder.
     *
     * @return AutofocusSettings.Builder with default settings.
     */
@@ -13,4 +14,15 @@ public class DefaultLightSheetManager implements LightSheetManager {
    public AutofocusSettings.Builder autofocusSettingsBuilder() {
       return new DefaultAutofocusSettings.Builder();
    }
+   
+   /**
+    * Access to a VolumeSettingsBuilder.
+    *
+    * @return VolumeSettings.Builder with default settings.
+    */
+   @Override
+   public VolumeSettings.Builder volumeSettingsBuilder() {
+      return new DefaultVolumeSettings.Builder();
+   }
+   
 }
