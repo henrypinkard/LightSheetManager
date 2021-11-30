1a08471e8658165ecd253d94268247404e964f48
bls337 <bls337@yahoo.com>
2021-11-29 17:48:19 -0800

    remove SliceSettings from VolumeSettings

--- src/test/java/org/micromanager/lightsheetmanager/internal/VolumeSettingsTest.java
+++ src/test/java/org/micromanager/lightsheetmanager/internal/VolumeSettingsTest.java
@@ -8,14 +8,11 @@ import org.micromanager.lightsheetmanager.VolumeSettings;
 import org.micromanager.lightsheetmanager.LightSheetManager;
 
 public class VolumeSettingsTest {
-    private final String firstSide_ = "B";
-    private final int numSides_ = 2;
+    private final String firstView_ = "B";
+    private final int numViews_ = 2;
     private final int numSlices_ = 50;
-    private final double sideDelayMs_ = 100;
+    private final double viewDelayMs_ = 100;
     private final double stepSizeUm_ = 0.5;
-    private final double slicePeriodMs_ = 50;
-    private final double sampleExposureMs_ = 8.5;
-    private final boolean minimizeSlicePeriod_ = true;
 
     private final double delta_ = 0.00001;
 
@@ -24,34 +21,25 @@ public class VolumeSettingsTest {
         LightSheetManager lsm = new DefaultLightSheetManager();
 
         VolumeSettings.Builder vsb = lsm.volumeSettingsBuilder();
-        vsb.firstSide(firstSide_)
-                .numSides(numSides_)
+        vsb.firstView(firstView_)
+                .numViews(numViews_)
                 .slicesPerVolume(numSlices_)
-                .delayBeforeSide(sideDelayMs_)
-                .sliceStepSize(stepSizeUm_)
-                .slicePeriod(slicePeriodMs_)
-                .sampleExposure(sampleExposureMs_)
-                .minimizeSlicePeriod(minimizeSlicePeriod_);
+                .delayBeforeView(viewDelayMs_)
+                .sliceStepSize(stepSizeUm_);
 
         VolumeSettings vs = vsb.build();
-        assertEquals(firstSide_, vs.firstSide());
-        assertEquals(numSides_, vs.numSides());
+        assertEquals(firstView_, vs.firstView());
+        assertEquals(numViews_, vs.numViews());
         assertEquals(numSlices_, vs.slicesPerVolume());
-        assertEquals(sideDelayMs_, vs.delayBeforeSide(), delta_);
+        assertEquals(viewDelayMs_, vs.delayBeforeView(), delta_);
         assertEquals(stepSizeUm_, vs.sliceStepSize(), delta_);
-        assertEquals(slicePeriodMs_, vs.slicePeriod(), delta_);
-        assertEquals(sampleExposureMs_, vs.sampleExposure(), delta_);
-        assertEquals(minimizeSlicePeriod_, vs.isSlicePeriodMinimized());
 
         vs = vs.copyBuilder().build();
-        assertEquals(firstSide_, vs.firstSide());
-        assertEquals(numSides_, vs.numSides());
+        assertEquals(firstView_, vs.firstView());
+        assertEquals(numViews_, vs.numViews());
         assertEquals(numSlices_, vs.slicesPerVolume());
-        assertEquals(sideDelayMs_, vs.delayBeforeSide(), delta_);
+        assertEquals(viewDelayMs_, vs.delayBeforeView(), delta_);
         assertEquals(stepSizeUm_, vs.sliceStepSize(), delta_);
-        assertEquals(slicePeriodMs_, vs.slicePeriod(), delta_);
-        assertEquals(sampleExposureMs_, vs.sampleExposure(), delta_);
-        assertEquals(minimizeSlicePeriod_, vs.isSlicePeriodMinimized());
     }
 
 }
