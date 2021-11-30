990091c0a9f1939ce4006f496050b46b3107f4ff
bls337 <bls337@yahoo.com>
2021-11-29 17:53:57 -0800

    remove SliceSettings from VolumeSettings
    rename side to view

--- src/main/java/org/micromanager/lightsheetmanager/VolumeSettings.java
+++ src/main/java/org/micromanager/lightsheetmanager/VolumeSettings.java
@@ -8,11 +8,11 @@ public interface VolumeSettings {
     interface Builder {
 
         /**
-         * Sets the number of sides to use during an acquisition.
+         * Sets the number of views to use during an acquisition.
          *
-         * @param numSides the number of sides
+         * @param numViews the number of views
          */
-        Builder numSides(final int numSides);
+        Builder numViews(final int numViews); // TODO: rename to numViews
 
         /**
          * Sets the number of slices per volume.
@@ -24,16 +24,16 @@ public interface VolumeSettings {
         /**
          * Sets the imaging path to start the acquisition with.
          *
-         * @param firstSide the first side
+         * @param firstView the first view
          */
-        Builder firstSide(final String firstSide);
+        Builder firstView(final String firstView); // TODO: rename to firstView
 
         /**
          * Sets the delay between switching imaging paths in milliseconds.
          *
-         * @param sideDelayMs the delay in milliseconds
+         * @param viewDelayMs the delay in milliseconds
          */
-        Builder delayBeforeSide(final double sideDelayMs);
+        Builder delayBeforeView(final double viewDelayMs); // TODO: rename to delayBeforeView
 
         /**
          * Sets the step size between each slice in microns.
@@ -41,27 +41,6 @@ public interface VolumeSettings {
          * @param stepSizeUm the step size in microns
          */
         Builder sliceStepSize(final double stepSizeUm);
-
-        /**
-         * Sets the slice period in milliseconds.
-         *
-         * @param slicePeriodMs the slice period in milliseconds
-         */
-        Builder slicePeriod(final double slicePeriodMs);
-
-        /**
-         * Sets the sample exposure time in milliseconds.
-         *
-         * @param exposureMs the exposure time in milliseconds
-         */
-        Builder sampleExposure(final double exposureMs);
-
-        /**
-         * Sets the slice period automatically.
-         *
-         * @param state true to minimize the slice period
-         */
-        Builder minimizeSlicePeriod(final boolean state);
 
         /**
          * Creates an immutable instance of VolumeSettings
@@ -79,11 +58,11 @@ public interface VolumeSettings {
     Builder copyBuilder();
 
     /**
-     * Return the number of sides to use during an acquisition.
+     * Return the number of views to use during an acquisition.
      *
-     * @return the number of sides
+     * @return the number of views
      */
-    int numSides();
+    int numViews();
 
     /**
      * Returns the number of slices per volume.
@@ -95,16 +74,16 @@ public interface VolumeSettings {
     /**
      * Returns the imaging path that the acquisition starts on.
      *
-     * @return the first side
+     * @return the first view
      */
-    String firstSide();
+    String firstView();
 
     /**
      * Return the delay in milliseconds before switching imaging paths in an acquisitions.
      *
      * @return the delay in milliseconds
      */
-    double delayBeforeSide();
+    double delayBeforeView();
 
     /**
      * Returns the step size in microns.
@@ -113,24 +92,4 @@ public interface VolumeSettings {
      */
     double sliceStepSize();
 
-    /**
-     * Returns the slice period in milliseconds.
-     *
-     * @return the slice period in milliseconds
-     */
-    double slicePeriod();
-
-    /**
-     * Returns the sample exposure time in milliseconds.
-     *
-     * @return the exposure time in milliseconds
-     */
-    double sampleExposure();
-
-    /**
-     * Returns true if the slice period is minimized.
-     *
-     * @return true if slice period is minimized
-     */
-    boolean isSlicePeriodMinimized();
 }
