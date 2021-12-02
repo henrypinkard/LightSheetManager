2176211a39125a4de562e02b955c910e0f5846af
bls337 <bls337@yahoo.com>
2021-12-02 11:37:24 -0800

    Tests that include volumeBounds

--- src/test/java/org/micromanager/lightsheetmanager/internal/VolumeSettingsTest.java
+++ src/test/java/org/micromanager/lightsheetmanager/internal/VolumeSettingsTest.java
@@ -9,10 +9,8 @@ import org.micromanager.lightsheetmanager.LightSheetManager;
 
 public class VolumeSettingsTest {
     private final String firstView_ = "B";
-    private final int numViews_ = 2;
-    private final int numSlices_ = 50;
     private final double viewDelayMs_ = 100;
-    private final double stepSizeUm_ = 0.5;
+    private final int numViews_ = 2;
 
     private final double delta_ = 0.00001;
 
@@ -20,26 +18,74 @@ public class VolumeSettingsTest {
     public void testVolumeSettings() {
         LightSheetManager lsm = new DefaultLightSheetManager();
 
+        final double startPosition = -4.0;
+        final double endPosition = 4.0;
+        final double stepSizeUm = 0.5;
+        final double expectedSlices = 16;
+        final double expectedCenter = 0;
+
+        // test the first volumeBound method
         VolumeSettings.Builder vsb = lsm.volumeSettingsBuilder();
-        vsb.firstView(firstView_)
+        vsb.volumeBounds(startPosition, endPosition, stepSizeUm)
+                .firstView(firstView_)
                 .numViews(numViews_)
-                .slicesPerVolume(numSlices_)
-                .delayBeforeView(viewDelayMs_)
-                .sliceStepSize(stepSizeUm_);
+                .delayBeforeView(viewDelayMs_);
 
         VolumeSettings vs = vsb.build();
         assertEquals(firstView_, vs.firstView());
         assertEquals(numViews_, vs.numViews());
-        assertEquals(numSlices_, vs.slicesPerVolume());
         assertEquals(viewDelayMs_, vs.delayBeforeView(), delta_);
-        assertEquals(stepSizeUm_, vs.sliceStepSize(), delta_);
+        assertEquals(startPosition, vs.startPosition(), delta_);
+        assertEquals(expectedCenter, vs.centerPosition(), delta_);
+        assertEquals(endPosition, vs.endPosition(), delta_);
+        assertEquals(stepSizeUm, vs.sliceStepSize(), delta_);
+        assertEquals(expectedSlices, vs.slicesPerVolume(), delta_);
 
+        // test the second volumeBound method
+        final int numSlices = 16;
+        final double expectedStepSize = 0.5;
+        vsb.volumeBounds(startPosition, endPosition, numSlices)
+                .firstView(firstView_)
+                .numViews(numViews_)
+                .delayBeforeView(viewDelayMs_);
+
+        vs = vsb.build();
+        assertEquals(firstView_, vs.firstView());
+        assertEquals(numViews_, vs.numViews());
+        assertEquals(viewDelayMs_, vs.delayBeforeView(), delta_);
+        assertEquals(startPosition, vs.startPosition(), delta_);
+        assertEquals(expectedCenter, vs.centerPosition(), delta_);
+        assertEquals(endPosition, vs.endPosition(), delta_);
+        assertEquals(expectedStepSize, vs.sliceStepSize(), delta_);
+        assertEquals(numSlices, vs.slicesPerVolume());
+
+        // test the third volumeBound method
+        final double centerPosition = 0.0;
+        vsb.volumeBounds(centerPosition, numSlices, stepSizeUm)
+                .firstView(firstView_)
+                .numViews(numViews_)
+                .delayBeforeView(viewDelayMs_);
+
+        vs = vsb.build();
+        assertEquals(firstView_, vs.firstView());
+        assertEquals(numViews_, vs.numViews());
+        assertEquals(viewDelayMs_, vs.delayBeforeView(), delta_);
+        assertEquals(startPosition, vs.startPosition(), delta_);
+        assertEquals(centerPosition, vs.centerPosition(), delta_);
+        assertEquals(endPosition, vs.endPosition(), delta_);
+        assertEquals(expectedStepSize, vs.sliceStepSize(), delta_);
+        assertEquals(numSlices, vs.slicesPerVolume());
+
+        // test copy builder method
         vs = vs.copyBuilder().build();
         assertEquals(firstView_, vs.firstView());
         assertEquals(numViews_, vs.numViews());
-        assertEquals(numSlices_, vs.slicesPerVolume());
         assertEquals(viewDelayMs_, vs.delayBeforeView(), delta_);
-        assertEquals(stepSizeUm_, vs.sliceStepSize(), delta_);
+        assertEquals(startPosition, vs.startPosition(), delta_);
+        assertEquals(centerPosition, vs.centerPosition(), delta_);
+        assertEquals(endPosition, vs.endPosition(), delta_);
+        assertEquals(expectedStepSize, vs.sliceStepSize(), delta_);
+        assertEquals(numSlices, vs.slicesPerVolume());
     }
 
 }
