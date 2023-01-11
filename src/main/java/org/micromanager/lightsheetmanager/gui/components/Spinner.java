b243b11e9011da3b91d0ad0f4036b99b055b7009
Brandon <brandon@asiimaging.com>
2023-01-11 14:12:02 -0800

    fix for channel table labels

--- src/main/java/org/micromanager/lightsheetmanager/gui/components/Spinner.java
+++ src/main/java/org/micromanager/lightsheetmanager/gui/components/Spinner.java
@@ -5,6 +5,8 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
+// TODO: use Math.max(min, Math.min(start, max)) to modify to start value, what if this invalidates and changes saved acquisition settings?
+
 public class Spinner extends JSpinner {
 
     private static int defaultSize = 5;
@@ -29,7 +31,7 @@ public class Spinner extends JSpinner {
             final Float min,
             final Float max,
             final Float step) {
-        return new Spinner(start, min, max, step);
+        return new Spinner(Math.max(min, Math.min(start, max)), min, max, step);
     }
 
     public static Spinner createIntegerSpinner(
@@ -45,7 +47,7 @@ public class Spinner extends JSpinner {
             final Double min,
             final Double max,
             final Double step) {
-        return new Spinner(start, min, max, step);
+        return new Spinner(Math.max(min, Math.min(start, max)), min, max, step);
     }
 
     public void setColumnSize(final int width) {
