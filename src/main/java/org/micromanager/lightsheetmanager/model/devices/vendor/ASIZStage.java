6d480db9d88f5fba4aad39e56ec22d3e5204579c
Brandon <brandon@asiimaging.com>
2023-04-10 10:51:51 -0700

    more PLogic code

--- src/main/java/org/micromanager/lightsheetmanager/model/devices/vendor/ASIZStage.java
+++ src/main/java/org/micromanager/lightsheetmanager/model/devices/vendor/ASIZStage.java
@@ -7,4 +7,45 @@ public class ASIZStage extends ASITigerBase {
     public ASIZStage(final Studio studio, final String deviceName) {
         super(studio, deviceName);
     }
+
+    public void setPosition(final double position) {
+        try {
+            core_.setPosition(deviceName_, position);
+        } catch (Exception e) {
+            studio_.logs().showError("could not set position for " + deviceName_);
+        }
+    }
+
+    public void setRelativePosition(final double position) {
+        try {
+            core_.setRelativePosition(deviceName_, position);
+        } catch (Exception e) {
+            studio_.logs().showError("could not set relative position for " + deviceName_);
+        }
+    }
+
+    public double getPosition() {
+        try {
+            return core_.getPosition(deviceName_);
+        } catch (Exception e) {
+            studio_.logs().showError("could not get position for " + deviceName_);
+            return 0.0;
+        }
+    }
+
+    public void setTTLInputMode(final String mode) {
+        setProperty("TTLInputMode", mode);
+    }
+
+    public String getTTLInputMode() {
+        return getProperty("TTLInputMode");
+    }
+
+    public static class Properties {
+
+    }
+
+//    public static enum TTLInputMode {
+//
+//    }
 }
